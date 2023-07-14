aaf935b5580d74a6c4a3705bf8df8610f27fb666
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-07-14 11:38:32 -0400

    working prompts, complete

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,5 +1,7 @@
 package io.zipcoder.polymorphism;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainApplication {
@@ -9,42 +11,46 @@ public class MainApplication {
 
         Scanner scanner = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-        sb.append("Enter what kind of pet you have then its name").append("\n")
-                .append("Accepted pets = Dog, Cat, Zebra").append("\n")
-                .append("Example: enter Dog Fido with the spaces to store your dog");
+        sb.append("Enter what kind of pet you have").append("\n")
+                .append("Accepted pets = Dog, Cat, Zebra");
 
         //Ask how many pets they have
         System.out.println("How many pets do you have?");
         Integer numberOfPets = scanner.nextInt();
-        Pet[] pets = new Pet[numberOfPets];
+        List<Pet> petList = new ArrayList<>();
 
         //Get which kind of pet each one is, and their names.
-        for (int i = 0; i <numberOfPets; i++){
+        for (int i = 0; i <numberOfPets + 1; i++){
 
             System.out.println(sb);
-            String thePet = scanner.next();
+            String thePet = scanner.nextLine();
 
-            String[] thePetArray = thePet.split(" ");
-            if (thePetArray[0].equalsIgnoreCase("dog")){
-//                StringBuilder animalName = new StringBuilder().append("Dog").append(i);
-                Dog dog = new Dog(thePetArray[1]);
-                pets[i] = dog;
+            if (thePet.equalsIgnoreCase("dog")){
+                System.out.println("What is your dog's name");
+                String dogName = scanner.nextLine();
+
+                Dog dog = new Dog(dogName);
+               petList.add(dog);
             }
-            if (thePetArray[0].equalsIgnoreCase("cat")){
-//                StringBuilder animalName = new StringBuilder().append("Dog").append(i);
-                Cat cat = new Cat(thePetArray[1]);
-                pets[i] = cat;
+            if (thePet.equalsIgnoreCase("cat")){
+                System.out.println("What is your cat's name");
+                String catName = scanner.nextLine();
+
+                Cat cat = new Cat(catName);
+                petList.add(cat);
             }
-            if (thePetArray[0].equalsIgnoreCase("Zebra")){
-//                StringBuilder animalName = new StringBuilder().append("Dog").append(i);
-                Zebra zebra = new Zebra(thePetArray[1]);
-                pets[i] = zebra;
+            if (thePet.equalsIgnoreCase("Zebra")){
+                System.out.println("A zebra? Ok...What is your zebra's name");
+                String zebraName = scanner.nextLine();
+
+                Zebra zebra = new Zebra(zebraName);
+                petList.add(zebra);
             }
         }
 //Print out a list of all the pets names and what they speak.
-        for(Pet pet : pets){
-            System.out.println(pet.getName());
-            System.out.println(pet.speak());
+        for(int i = 0; i < petList.size(); i++){
+            System.out.println(petList.get(i).getName());
+            System.out.println(petList.get(i).speak() + "\n");
 
         }
 
